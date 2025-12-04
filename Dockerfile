14944eea02ac04bf4df88d835667e25dd5a4f946
NamPE286 <nambuihung654@gmail.com>
2025-12-05 04:12:15 +0700

    deploy

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM oven/bun:1
+
+WORKDIR /usr/src/app
+
+COPY . .
+
+RUN bun install
+
+USER bun
+EXPOSE 8080
+ENTRYPOINT [ "bun", "run", "deploy-start" ]
--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,4 +1,3 @@
-import 'dotenv/config';
 import { REST, Routes } from 'discord.js';
 import { readdirSync } from 'fs';
 import { join, dirname } from 'path';
